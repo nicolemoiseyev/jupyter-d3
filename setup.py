356425bdb2d46aeabff1e6e1d120a16c19cff641
NICOLE MOISEYEV <nicole.moiseyev3400@gmail.com>
2020-06-17 13:00:05 -0400

    Created basic package structure files and folders

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+setup(
+  name='jupyter_d3',
+  version='0.0.1',
+  url='https://github.com/nicolemoiseyev/jupyter-d3/',
+  author='Nicole Moiseyev',
+  author_email='nicole.moiseyev@duke.edu',
+  long_description=open('README.md', 'r').read(),
+  license='MIT',
+  install_requires=list(map(str.strip, open('requirements.txt', 'r').readlines())),
+  packages=find_packages(),
+  include_package_data=True,
+)
